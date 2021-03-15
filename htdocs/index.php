bace009dd43e2eb6720f91a49945f940b1a931bf
Bart Moorman <github@id10t.us>
2021-03-15 10:31:02 -0600

    Put canvas inside div

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,7 +12,9 @@ $shortener = new Shortener(true, true, false, false);
   </head>
   <body>
 <?php require_once('header.php'); ?>
-    <canvas id='chart'></canvas>
+    <div class='container-fluid'>
+      <canvas id='chart'></canvas>
+    </div>
 <?php require_once('include.js'); ?>
     <script src='//cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.min.js' integrity='sha384-F13mJAeqdsVJS5kJv7MZ4PzYmJ+yXXZkt/gEnamJGTXZFzYgAcVtNg5wBDrRgLg9' crossorigin='anonymous'></script>
     <script src='//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js' integrity='sha384-0saKbDOWtYAw5aP4czPUm6ByY5JojfQ9Co6wDgkuM7Zn+anp+4Rj92oGK8cbV91S' crossorigin='anonymous'></script>
